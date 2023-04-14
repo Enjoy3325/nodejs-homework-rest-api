5f3c7edeea80e4bdc23599e25a32a8c2fd6f6f6d
Enjoy3325 <enjoy3325@gmail.com>
2023-04-14 16:11:11 +0300

    routes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
-
-const {contactRouter, userRouter } = require('./routes/api')
+const router = require('./routes/api/contacts/contacts-routes')
+// const {contactRouter, userRouter } = require('./routes/api')
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -16,8 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/users", userRouter.userRouter)
-app.use("/api/contacts", contactRouter.contactRouter)
+// app.use("/api/users", userRouter.userRouter)
+app.use("/api/contacts", router)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,15 +3,28 @@ const { Contact } = require('../../models/contact/contact');
 const listContacts = async (req, res) => {
   console.log(req.user);
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  limit = +limit > 20 ? 20 : +limit; 
-  const skip = +page > 1 ? +limit * (+page - 1) : 0;
-  const result = await Contact.find({ owner })
-    .populate("owner", "name email")
-    .skip(skip )
-  .limit(limit)
-  
-  res.json({result,  page: +page, limit })
-}
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  try {
+    if (favorite) {
+      const result = await Contact.find(
+        { owner, favorite },
+        {
+          skip,
+          limit,
+        }
+        ).populate("owner", "email");
+        res.json(result);
+       
+    }
+    const result = await Contact.find({ owner }, {
+      skip,
+      limit,
+    }).populate("owner", "email");
+    res.json(result);
+  } catch (error) {
+    throw HttpError(404, `Not found`);
+  }
+};
 
 module.exports = listContacts;
